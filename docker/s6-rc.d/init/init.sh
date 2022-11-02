3d9a3209f0a50aa516fd08ed6f5bc1076897b144
Daniel Ursache Dogariu <contact@danniel.net>
2022-11-02 12:15:11 +0200

    Check if superuser already exists before creating

--- docker/s6-rc.d/init/init.sh
+++ docker/s6-rc.d/init/init.sh
@@ -24,10 +24,17 @@ if [ "${RUN_FEED}" = "yes" ]; then
 fi
 
 if [ "${RUN_CREATE_SUPER_USER}" = "yes" ]; then
-    echo "Create superuser"
-    python3 manage.py createsuperuser --noinput \
-        --username "${DJANGO_ADMIN_EMAIL}" --email "${DJANGO_ADMIN_EMAIL}"
+    echo "Check superuser"
+    SUPERUSERS=$(python3 manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username=\"${DJANGO_ADMIN_EMAIL}\").count())")
 
-    echo "Set superuser password"
-    python3 manage.py shell -c "from django.contrib.auth.models import User; u = User.objects.get(username=\"${DJANGO_ADMIN_EMAIL}\"); u.set_password(\"${DJANGO_ADMIN_PASSWORD}\"); u.save()"
+    if [ "${SUPERUSERS}" = "0" ]; then
+        echo "Create superuser"
+        python3 manage.py createsuperuser --noinput \
+            --username "${DJANGO_ADMIN_EMAIL}" --email "${DJANGO_ADMIN_EMAIL}"
+
+        echo "Set superuser password"
+        python3 manage.py shell -c "from django.contrib.auth.models import User; u = User.objects.get(username=\"${DJANGO_ADMIN_EMAIL}\"); u.set_password(\"${DJANGO_ADMIN_PASSWORD}\"); u.save()"
+    else
+        echo "Superuser already exists"
+    fi
 fi
