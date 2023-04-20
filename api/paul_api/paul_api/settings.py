483e3d783367be32203876523ae4c0bdb9402ff8
Daniel Ursache Dogariu <contact@danniel.net>
2023-04-20 22:46:20 +0300

    Disable frontend session auth and only keep the token auth

--- api/paul_api/paul_api/settings.py
+++ api/paul_api/paul_api/settings.py
@@ -307,7 +307,7 @@ elif USE_AZURE:
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
