4abfdff239066cdb12cee67287c1217b001091b9
Daniel Ursache Dogariu <contact@danniel.net>
2024-09-10 01:31:11 +0300

    Customize the navigation for regular users

--- backend/dashboard/admin.py
+++ backend/dashboard/admin.py
@@ -4,10 +4,19 @@ from django.utils.translation import gettext_lazy as _
 
 class PaulAdminSite(UnfoldAdminSite):
     index_title = _("Welcome to Paul")
-    index_template = "account_index.html"
+    index_template = "dashboard/index.html"
     site_header = _("Paul")
     site_title = _("Paul")
     site_url = None
+
+    def each_context(self, request):
+        context = super().each_context(request)
+        context.update(
+            {
+                "app_dashboard": True,
+            }
+        )
+        return context
 
 
 dashboard_site = PaulAdminSite(name="app")
